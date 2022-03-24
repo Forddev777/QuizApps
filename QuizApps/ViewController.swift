d8d525db05a68f36e75c243791fe2b3dc9faada8
Forddev777 <taothongdev@gmail.com>
2022-03-24 22:56:35 +0700

    update

--- QuizApps/ViewController.swift
+++ QuizApps/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressbar: UIProgressView!
+    @IBOutlet weak var falseBtn: UIButton!
+    @IBOutlet weak var trueBtn: UIButton!
+    
+    let quiz = [
+           [ "1ONE + Two equal to Six", "True"],
+           [ "2Two + Two is greater than One", "True"],
+           [ "3Three + Two equal to Five" ,  "False"]
+    ]
+    
+    var questionNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
+        
     }
+    
 
+    @IBAction func answerBtnPress(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle // true , false
+        let actualAnswer = quiz[questionNumber][1]
+        
+        
+        if userAnswer  == actualAnswer {
+                print("Right!")
+        }else{
+            
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count    {
+            questionNumber += 1
+        }else{
+            
+            questionNumber = 0
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+        
+        
+    }
+    
+    
 
+    
+    
+    
 }
 
---
+++ QuizApps/quiz_data.swift
@@ -0,0 +1,16 @@
+//
+//  quiz_data.swift
+//  QuizApps
+//
+//  Created by suriya taothongkom on 24/3/2565 BE.
+//
+
+import Foundation
+
+
+struct quiz_name {
+    
+         let question = ""
+         
+
+}
